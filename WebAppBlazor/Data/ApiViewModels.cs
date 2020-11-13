ae6dffb230cc152b150d7a048c06a7daa406e969
Anna Poddubnaya <anya.olehovna@gmail.com>
2020-11-13 17:42:59 +0300

    Finished Lab 10

---
+++ WebAppBlazor/Data/ApiViewModels.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebAppBlazor.Data
+{
+    public class CertificateListViewModel
+    {
+        [JsonPropertyName("certificateId")]
+        public int CertificateId { get; set; }
+        [JsonPropertyName("certificateName")]
+        public string CertificateName { get; set; }
+    }
+    public class CertificateDetailsViewModel
+    {
+        [JsonPropertyName("certificateName")]
+        public string CertificateName { get; set; }
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+        [JsonPropertyName("price")]
+        public int Price { get; set; }
+        [JsonPropertyName("image")]
+        public string Image { get; set; }
+    }
+
+}
